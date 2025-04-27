a31ecdad90501efc33fa10cf5d5fc55c2b4694a3
Tom Seidel <hi@tomseidel.com>
2025-04-27 17:33:24 +0200

    refactor: introduce new files and update div elements to section

---
+++ full-sites/cloud-dot/js/index.js
@@ -0,0 +1,7 @@
+// JavaScript to toggle the mobile menu
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
